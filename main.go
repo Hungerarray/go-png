2060b5260bdaeb990418226c0a1e5ac791740e26
Ranjan Ojha <ojha@zhaw.com>
2024-07-16 23:28:45 +0200

    add: short hand flag

--- main.go
+++ main.go
@@ -9,17 +9,23 @@ import (
 	"github.com/hungerarray/go-png/png"
 )
 
+var filePath string
+
+func init() {
+	flag.StringVar(&filePath, "filepath", "", "png file path")
+	flag.StringVar(&filePath, "f", "", "png file path (short)")
+}
+
 func main() {
-	filePath := flag.String("filepath", "", "png file path")
 	flag.Parse()
 
-	if filePath == nil || *filePath == "" {
+	if filePath == "" {
 		fmt.Fprintf(os.Stderr, "usage: %s\n", os.Args[0])
 		flag.PrintDefaults()
 		os.Exit(-1)
 	}
 
-	png, err := png.NewPng(*filePath)
+	png, err := png.NewPng(filePath)
 	if err != nil {
 		log.Fatalf("[error]: %s\n", err.Error())
 	}
